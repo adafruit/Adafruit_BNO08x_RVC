6dc29691c29c3dfb5cf77fac35dc7c0c08da0412
siddacious <nospam187+github@gmail.com>
2020-09-17 15:43:48 -0700

    tweaked example

--- examples/uart_rvc/uart_rvc.ino
+++ examples/uart_rvc/uart_rvc.ino
@@ -1,26 +1,12 @@
-#if defined(__AVR__) || defined(ESP8266)
-// For UNO and others without hardware serial, we must use software serial...
-// pin #2 is IN from sensor (WHITE wire)
-// Set up the serial port to use softwareserial..
-#include <SoftwareSerial.h>
-SoftwareSerial mySerial(2, -1);
-// void parseFrame(uint8_t * buffer, size_t len);
-#else
-// On Leonardo/M0/etc, others with hardware serial, use hardware serial!
-// #0 is white wire, data input
 #define mySerial Serial1
 
-#endif
 #define MILLI_G_TO_MS2 0.0098067
 #define DEGREE_SCALE 0.01
 #define BUFFER_SIZE 64
 int16_t raw_accel_x, raw_accel_y, raw_accel_z, raw_yaw, raw_roll,
-    raw_pitch;               // The last measured distance
-bool newData = false;        // Whether new data is available from the sensor
-uint8_t buffer[BUFFER_SIZE]; // our buffer for storing data
-uint8_t idx = 0;             // our idx into the storage buffer
+    raw_pitch;               
+uint8_t buffer[BUFFER_SIZE];
 float yaw, pitch, roll, x_accel, y_accel, z_accel;
-bool started = false;
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(115200);
@@ -29,7 +15,7 @@ void setup() {
         10); // wait for serial port to connect. Needed for native USB port only
   }
 
-  Serial.println("Adafruit BNO08x UART-RVC Test");
+//  Serial.println("Adafruit BNO08x UART-RVC Test");
 
   // set the data rate for the Serial port, 9600 for the sensor
   mySerial.begin(115200);
@@ -91,23 +77,22 @@ bool parseFrame() {
 }
 
 void printFrame() {
-  Serial.print("Yaw: ");
+  Serial.print("Yaw:");
   Serial.print(yaw);
-  Serial.print(" Pitch: ");
+  Serial.print(", Pitch:");
   Serial.print(pitch);
-  Serial.print(" Roll: ");
+  Serial.print(", Roll: ");
   Serial.print(roll);
-  Serial.println(" degrees");
+//  Serial.println(" degrees");
 
-  Serial.print("Acceleration:");
-  Serial.print("X: ");
+//  Serial.print("Acceleration:");
+  Serial.print(", X:");
   Serial.print(x_accel);
-  Serial.print(" Y: ");
+  Serial.print(", Y:");
   Serial.print(y_accel);
-  Serial.print(" Z: ");
+  Serial.print(", Z:");
   Serial.print(z_accel);
 
-  Serial.println(" m/s^2");
+//  Serial.println(" m/s^2");
   Serial.println("");
 }
-
